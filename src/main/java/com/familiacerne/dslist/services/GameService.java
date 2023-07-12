ab2ff6c9f56fdb5104ae9f819d8ee1f5cb01fb66
cassiotsantos <psyteqkilla@gmail.com>
2023-07-12 18:33:29 -0300

    get by id

--- src/main/java/com/familiacerne/dslist/services/GameService.java
+++ src/main/java/com/familiacerne/dslist/services/GameService.java
@@ -1,26 +1,34 @@
 package com.familiacerne.dslist.services;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import com.familiacerne.dslist.dto.GameDTO;
 import com.familiacerne.dslist.dto.GameMInDTO;
+import com.familiacerne.dslist.entities.Game;
 import com.familiacerne.dslist.repositories.GameRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
-
 public class GameService {
 	
 	@Autowired
 	private GameRepository gameRepository;
 	
+	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+		
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMInDTO> findAll() {
 		var result = gameRepository.findAll();
 		List<GameMInDTO> dto = result.stream().map(x -> new GameMInDTO(x)).toList();
 		return dto;		
 	}
-	
 	
 
 }
